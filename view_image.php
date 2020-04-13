9b64cd960831c558f504c1c6dc01229fb839c9f4
Ben Ryan <benryan03@gmail.com>
2020-04-12 22:42:42 -0400

    Created view_image.php

---
+++ view_image.php
@@ -0,0 +1,73 @@
+<?php
+
+//Get logged in user. If no user is logged in, redirect to login page.
+session_start();
+if (!isset($_SESSION["loggedInUser"])){
+    header("Location:login.php");}
+else{
+    $loggedInUser = $_SESSION["loggedInUser"];}
+
+//Connect to database
+$serverName = "localhost\sqlexpress";
+$connectionInfo = array("Database"=>"social_network", "UID"=>"ben", "PWD"=>"password123");
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+//Get userID of loggedInUser
+$getCurrentUserIDQuery = "SELECT id FROM users WHERE username = '$loggedInUser'";
+$getCurrentUserID = sqlsrv_query($conn, $getCurrentUserIDQuery, array());
+$currentUserID = sqlsrv_fetch_array($getCurrentUserID);
+$currentUserID = $currentUserID[0];
+
+//Get pending friend requests of loggedInUser
+$getPendingRequestsQuery = "SELECT * FROM friends WHERE friendid = '$currentUserID' AND accepted = 'False'";
+$getPendingRequests = sqlsrv_query($conn, $getPendingRequestsQuery, array(), array( "Scrollable" => 'static' ));
+$pendingRequestsCount = sqlsrv_num_rows($getPendingRequests);
+
+
+$selectedImage = $_GET["selectedImage"];
+
+
+?>
+
+<html>
+<head>
+    <title>Welcome to Social Network</title>
+    <link rel="stylesheet" type="text/css" href="default.css">
+</head>
+<body>
+    <center>
+    <!--Logo-->
+    <div class="header" id="header">
+        <h1><a href="index.php">Social Network</a></h1>
+    </div>
+
+    <!--Options bar-->
+    <div id="options">
+        <span id="search">   
+            <?php
+                echo '<form action="search.php" method="post" style="display: inline;"><input type="text" name="search" placeholder="Search"><input type="submit" value="Submit" name="submitSearch"></form>';
+                if ($pendingRequestsCount > 0){
+                    echo '&nbsp;&nbsp;<a href="requests.php" id="requests">'.$pendingRequestsCount.' new friend requests</a>';
+                }
+            ?>
+        </span>
+        <span id="userOptions">
+            <?php if (!isset($_SESSION["loggedInUser"])){echo '<a href="register.php">Register</a>&nbsp;';} ?>
+            <?php if (!isset($_SESSION["loggedInUser"])){echo '<a href="login.php"44>Log in</a>&nbsp;';} ?>
+            <?php if (isset($_SESSION["loggedInUser"])){echo '<a href="logout.php">Log out</a>';} ?>
+            <?php if (isset($_SESSION["loggedInUser"])){echo 'Welcome, <a href="profile.php?selectedUser=' . $loggedInUser . '">' .$loggedInUser. '</a>';}
+            ?>
+        </span>
+    </div>
+
+    <!--Image-->
+    <div class="contents">
+        <span id="image">
+            <?php
+               echo $selectedImage;
+            ?>
+        </div>
+    </div>
+    </center>
+</body>
+</html>
