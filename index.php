9d8ece44f8be115d74c64fe6ed975e3b4309d13a
Ben Ryan <benryan03@gmail.com>
2020-04-16 22:17:59 -0400

    add links to images

--- index.php
+++ index.php
@@ -225,7 +225,7 @@ if (isset($_GET["unLikePost"])){
                 echo    "<font color='gray' size='2'>" . date_format($posts_array_row[3], "m/d/Y h:ia") . "</font><br>";
                         
                         //Post content
-                        if ($posts_array_row[4] != ""){echo "<img src='" . $posts_array_row[4] . "'><br>";}
+                        if ($posts_array_row[4] != ""){echo "<a href='view_image.php?selectedImage=" . substr(strval($posts_array_row[4]), 7) . "'><img src='" . $posts_array_row[4] . "'></a><br>";}
                         else {echo $posts_array_row[1] . "<br><font size='2'>";}
 
                         //Get number of likes
