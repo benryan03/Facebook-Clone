f30941ed8287545f6edbd61a40f90f83561ba83e
Ben Ryan <benryan03@gmail.com>
2020-04-23 18:09:45 -0400

    removed vestigial loop

--- index.php
+++ index.php
@@ -214,130 +214,126 @@ if (isset($_GET["page"])){
         $query = "SELECT * FROM posts WHERE post_author_id IN ($currentUserFriendsString) AND comment_of IS NULL OR post_author = '$loggedInUser' AND comment_of IS NULL ORDER BY date_submitted DESC OFFSET $offset ROWS FETCH NEXT 10 ROWS ONLY";
         $posts_array = sqlsrv_query($conn, $query, array(), array( "Scrollable" => 'static'));
 
-        for ($x = 1; $x < 11;){
+        for ($x = 1; $x < 11; $x++){
             $posts_array_row = sqlsrv_fetch_array($posts_array, SQLSRV_FETCH_NUMERIC); //Select next row in $query
-            
-            if ($posts_array_row[10] == null){ //if post is not a post comment
-                $x++;
-                //Display OP
-                echo "<div class='status'>";
-                echo    "<span class='profileThumb'>";
-                echo        "<a href='profile.php?selectedUser=" . $posts_array_row[2] . "'><img src='";
-                            //Get profile pic or null
-                            $getProfilePicQuery = "SELECT profile_pic FROM users WHERE username = '$posts_array_row[2]'";
-                            $getProfilePic = sqlsrv_query($conn, $getProfilePicQuery, array());
-                            $profilePic = sqlsrv_fetch_array($getProfilePic);
+            //Display OP
+            echo "<div class='status'>";
+            echo    "<span class='profileThumb'>";
+            echo        "<a href='profile.php?selectedUser=" . $posts_array_row[2] . "'><img src='";
+                        //Get profile pic or null
+                        $getProfilePicQuery = "SELECT profile_pic FROM users WHERE username = '$posts_array_row[2]'";
+                        $getProfilePic = sqlsrv_query($conn, $getProfilePicQuery, array());
+                        $profilePic = sqlsrv_fetch_array($getProfilePic);
 
-                            //If profile pic exists, display it. Else, display default profile pic.
-                            if ($profilePic[0] != null){echo "images/" . $profilePic[0];}                           
-                            else {echo "images\default_profile_picture_32.jpg";}
-                echo        "'></a>";
-                echo    "</span>";
-                    
-                echo    "<span class='statusContent'>";
-                            //Username of post author
-                echo        "<font color='#0080ff'><b><a href='profile.php?selectedUser=" . $posts_array_row[2] . "'>" . $posts_array_row[2]. "</a>" . "</b></font> ";
-
-                            //If post is not on author's wall, display username of that user
-                            if ($posts_array_row[7] != $posts_array_row[8]){echo "<font color='#0080ff'><b></a>" . " > " . "<a href='profile.php?selectedUser=" . $posts_array_row[9] . "'>" . $posts_array_row[9] . "</a>" . "</b></font></br> ";}
-                            else {echo "<br>";} 
-                            
-                            //Date posted
-                echo        "<font color='gray' size='2'>" . date_format($posts_array_row[3], "m/d/Y h:ia") . "</font><br>";
-                        
-                            //Post content
-                            if ($posts_array_row[4] != " "){echo "<a href='view_image.php?selectedImage=" . substr(strval($posts_array_row[4]), 7) . "'><img src='" . $posts_array_row[4] . "'></a><br><font size='2'>";}
-                            else {echo $posts_array_row[1] . "<br><font size='2'>";}
-
-                            //Get number of likes
-                            $getLikesQuery = "SELECT * FROM likes WHERE post_id = '$posts_array_row[0]'";
-                            $getLikes = sqlsrv_query($conn, $getLikesQuery, array(), array( "Scrollable" => 'static' ));
-                            $likesCount = sqlsrv_num_rows($getLikes);
-
-                            //Convert users who liked current post to array
-                            $likesArray = array();
-                            for ($y = 1; $y < $likesCount + 1; $y++){
-                                $likesRow = sqlsrv_fetch_array($getLikes, SQLSRV_FETCH_NUMERIC); //Select next row
-                                array_push($likesArray, $likesRow[1]);}
-
-                            if ($likesCount == 1) {echo "<div class='tooltip'>1 like<span class='tooltiptext'>" . implode(" ,", $likesArray) . "</span></div>&nbsp;";}
-                            else if ($likesCount > 1) {echo "<div class='tooltip'>" . $likesCount . "&nbsp;likes<span class='tooltiptext'>" . implode(", ", $likesArray) . "</span></div>&nbsp;";}
-
-                            //Like/unlike button
-                            if (!in_array($loggedInUser, $likesArray)){echo "<a href='?likePost=" . $posts_array_row[0] . "'>Like</a>&nbsp";}
-                            else {echo "<a href='?unLikePost=" . $posts_array_row[0] . "'>Unlike</a>&nbsp";}
-
-                            //Comment button/box
-                            if (isset($_GET["commentOn"]) && $_GET["commentOn"] == $posts_array_row[0]) { echo
-                                "<form action='?commentOn=" . $posts_array_row[0] . "' method='post'>" .
-                                "<input type='text' name='comment' placeholder='Add a comment'>" . 
-                                "<input type='submit' value='Submit' name='submitComment'><br>" . 
-                                "</form>";
-                            }
-                            else {echo "<a href='?commentOn=" . $posts_array_row[0] . "'>Comment</a>";}
-                echo        "</font>";
-                echo    "</span><br><br>";
-
-                        //Count how many comments the post has
-                        $comments_array = sqlsrv_query($conn, "SELECT * FROM posts WHERE comment_of = '$posts_array_row[0]' ", array(), array( "Scrollable" => 'static'));
-                        $comments_count = sqlsrv_num_rows($comments_array);
-                    
-                                        
-                        if ($comments_count > 0){
-                            for ($z = 1; $z < $comments_count + 1; $z++){
-                                $comments_array_row = sqlsrv_fetch_array($comments_array, SQLSRV_FETCH_NUMERIC); //Select next row in $query
-                //Display comment
-                echo"   <div class='statusComment'>";
-                echo        "<span class='commentProfileThumb'>";
-                echo            "<a href='profile.php?selectedUser=" . $comments_array_row[2] . "'><img src='";
-                                //Get profile pic or null
-                                $getProfilePicQuery2 = "SELECT profile_pic FROM users WHERE username = '$comments_array_row[2]'";
-                                $getProfilePic2 = sqlsrv_query($conn, $getProfilePicQuery2, array());
-                                $profilePic2 = sqlsrv_fetch_array($getProfilePic2);
-        
-                                //If profile pic exists, display it. Else, display default profile pic.
-                                if ($profilePic2[0] != null){echo "images/" . $profilePic2[0];}                           
-                                else {echo "images\default_profile_picture_32.jpg";}
-                echo            "'></a>";
-                echo        "</span>";            
-                echo        "<span class='commentContent'>";
-                                //Username of post author
-                echo            "<font color='#0080ff'><b><a href='profile.php?selectedUser=" . $comments_array_row[2] . "'>" . $comments_array_row[2]. "</a>" . "</b></font><br> ";
+                        //If profile pic exists, display it. Else, display default profile pic.
+                        if ($profilePic[0] != null){echo "images/" . $profilePic[0];}                           
+                        else {echo "images\default_profile_picture_32.jpg";}
+            echo        "'></a>";
+            echo    "</span>";
                 
-                                //Date posted
-                echo            "<font color='gray' size='2'>" . date_format($comments_array_row[3], "m/d/Y h:ia") . "</font><br>";
-                                    
-                                //Post content
-                                if ($comments_array_row[4] != ""){echo "<a href='view_image.php?selectedImage=" . substr(strval($comments_array_row[4]), 7) . "'><img src='" . $comments_array_row[4] . "'></a><br>";}
-                                else {echo $comments_array_row[1] . "<br>";}
+            echo    "<span class='statusContent'>";
+                        //Username of post author
+            echo        "<font color='#0080ff'><b><a href='profile.php?selectedUser=" . $posts_array_row[2] . "'>" . $posts_array_row[2]. "</a>" . "</b></font> ";
+
+                        //If post is not on author's wall, display username of that user
+                        if ($posts_array_row[7] != $posts_array_row[8]){echo "<font color='#0080ff'><b></a>" . " > " . "<a href='profile.php?selectedUser=" . $posts_array_row[9] . "'>" . $posts_array_row[9] . "</a>" . "</b></font></br> ";}
+                        else {echo "<br>";} 
                         
-                                //Get number of likes
-                                $getLikesQuery2 = "SELECT * FROM likes WHERE post_id = '$comments_array_row[0]'";
-                                $getLikes2 = sqlsrv_query($conn, $getLikesQuery2, array(), array( "Scrollable" => 'static' ));
-                                $likesCount2 = sqlsrv_num_rows($getLikes2);
-        
-                                //Convert users who liked current post to array
-                                $likesArray2 = array();
-                                for ($a = 1; $a < $likesCount2 + 1; $a++){
-                                    $likesRow2 = sqlsrv_fetch_array($getLikes2, SQLSRV_FETCH_NUMERIC); //Select next row
-                                    array_push($likesArray2, $likesRow2[1]);}
+                        //Date posted
+            echo        "<font color='gray' size='2'>" . date_format($posts_array_row[3], "m/d/Y h:ia") . "</font><br>";
+                    
+                        //Post content
+                        if ($posts_array_row[4] != " "){echo "<a href='view_image.php?selectedImage=" . substr(strval($posts_array_row[4]), 7) . "'><img src='" . $posts_array_row[4] . "'></a><br><font size='2'>";}
+                        else {echo $posts_array_row[1] . "<br><font size='2'>";}
 
-                                echo "<font size='2'>";
-                                if ($likesCount2 == 1) {echo "<div class='tooltip'>1 like<span class='tooltiptext'>" . implode(" ,", $likesArray2) . "</span></div>&nbsp;";}
-                                else if ($likesCount2 > 1) {echo "<div class='tooltip'>" . $likesCount2 . "&nbsp;likes<span class='tooltiptext'>" . implode(", ", $likesArray2) . "</span></div>&nbsp;";}
-        
-                                //Like/unlike button
-                                if (!in_array($loggedInUser, $likesArray2)){echo "<a href='?likePost=" . $comments_array_row[0] . "'>Like</a>&nbsp";}
-                                else {echo "<a href='?unLikePost=" . $comments_array_row[0] . "'>Unlike</a>&nbsp";}
-                                echo "</font>";
+                        //Get number of likes
+                        $getLikesQuery = "SELECT * FROM likes WHERE post_id = '$posts_array_row[0]'";
+                        $getLikes = sqlsrv_query($conn, $getLikesQuery, array(), array( "Scrollable" => 'static' ));
+                        $likesCount = sqlsrv_num_rows($getLikes);
 
-                                echo "<font size='1'><br><br></font>";
-                echo        "</span>";
-                echo    "</div>";          
-                            }
+                        //Convert users who liked current post to array
+                        $likesArray = array();
+                        for ($y = 1; $y < $likesCount + 1; $y++){
+                            $likesRow = sqlsrv_fetch_array($getLikes, SQLSRV_FETCH_NUMERIC); //Select next row
+                            array_push($likesArray, $likesRow[1]);}
+
+                        if ($likesCount == 1) {echo "<div class='tooltip'>1 like<span class='tooltiptext'>" . implode(" ,", $likesArray) . "</span></div>&nbsp;";}
+                        else if ($likesCount > 1) {echo "<div class='tooltip'>" . $likesCount . "&nbsp;likes<span class='tooltiptext'>" . implode(", ", $likesArray) . "</span></div>&nbsp;";}
+
+                        //Like/unlike button
+                        if (!in_array($loggedInUser, $likesArray)){echo "<a href='?likePost=" . $posts_array_row[0] . "'>Like</a>&nbsp";}
+                        else {echo "<a href='?unLikePost=" . $posts_array_row[0] . "'>Unlike</a>&nbsp";}
+
+                        //Comment button/box
+                        if (isset($_GET["commentOn"]) && $_GET["commentOn"] == $posts_array_row[0]) { echo
+                            "<form action='?commentOn=" . $posts_array_row[0] . "' method='post'>" .
+                            "<input type='text' name='comment' placeholder='Add a comment'>" . 
+                            "<input type='submit' value='Submit' name='submitComment'><br>" . 
+                            "</form>";
                         }
-                echo "</div><br><br>";
-            }
+                        else {echo "<a href='?commentOn=" . $posts_array_row[0] . "'>Comment</a>";}
+            echo        "</font>";
+            echo    "</span><br><br>";
+
+                    //Count how many comments the post has
+                    $comments_array = sqlsrv_query($conn, "SELECT * FROM posts WHERE comment_of = '$posts_array_row[0]' ", array(), array( "Scrollable" => 'static'));
+                    $comments_count = sqlsrv_num_rows($comments_array);
+                
+                                    
+                    if ($comments_count > 0){
+                        for ($z = 1; $z < $comments_count + 1; $z++){
+                            $comments_array_row = sqlsrv_fetch_array($comments_array, SQLSRV_FETCH_NUMERIC); //Select next row in $query
+                    //Display comment
+            echo    "<div class='statusComment'>";
+            echo        "<span class='commentProfileThumb'>";
+            echo            "<a href='profile.php?selectedUser=" . $comments_array_row[2] . "'><img src='";
+                            //Get profile pic or null
+                            $getProfilePicQuery2 = "SELECT profile_pic FROM users WHERE username = '$comments_array_row[2]'";
+                            $getProfilePic2 = sqlsrv_query($conn, $getProfilePicQuery2, array());
+                            $profilePic2 = sqlsrv_fetch_array($getProfilePic2);
+    
+                            //If profile pic exists, display it. Else, display default profile pic.
+                            if ($profilePic2[0] != null){echo "images/" . $profilePic2[0];}                           
+                            else {echo "images\default_profile_picture_32.jpg";}
+            echo            "'></a>";
+            echo        "</span>";            
+            echo        "<span class='commentContent'>";
+                            //Username of post author
+            echo            "<font color='#0080ff'><b><a href='profile.php?selectedUser=" . $comments_array_row[2] . "'>" . $comments_array_row[2]. "</a>" . "</b></font><br> ";
+            
+                            //Date posted
+            echo            "<font color='gray' size='2'>" . date_format($comments_array_row[3], "m/d/Y h:ia") . "</font><br>";
+                                
+                            //Post content
+                            if ($comments_array_row[4] != ""){echo "<a href='view_image.php?selectedImage=" . substr(strval($comments_array_row[4]), 7) . "'><img src='" . $comments_array_row[4] . "'></a><br>";}
+                            else {echo $comments_array_row[1] . "<br>";}
+                    
+                            //Get number of likes
+                            $getLikesQuery2 = "SELECT * FROM likes WHERE post_id = '$comments_array_row[0]'";
+                            $getLikes2 = sqlsrv_query($conn, $getLikesQuery2, array(), array( "Scrollable" => 'static' ));
+                            $likesCount2 = sqlsrv_num_rows($getLikes2);
+    
+                            //Convert users who liked current post to array
+                            $likesArray2 = array();
+                            for ($a = 1; $a < $likesCount2 + 1; $a++){
+                                $likesRow2 = sqlsrv_fetch_array($getLikes2, SQLSRV_FETCH_NUMERIC); //Select next row
+                                array_push($likesArray2, $likesRow2[1]);}
+
+                            echo "<font size='2'>";
+                            if ($likesCount2 == 1) {echo "<div class='tooltip'>1 like<span class='tooltiptext'>" . implode(" ,", $likesArray2) . "</span></div>&nbsp;";}
+                            else if ($likesCount2 > 1) {echo "<div class='tooltip'>" . $likesCount2 . "&nbsp;likes<span class='tooltiptext'>" . implode(", ", $likesArray2) . "</span></div>&nbsp;";}
+    
+                            //Like/unlike button
+                            if (!in_array($loggedInUser, $likesArray2)){echo "<a href='?likePost=" . $comments_array_row[0] . "'>Like</a>&nbsp";}
+                            else {echo "<a href='?unLikePost=" . $comments_array_row[0] . "'>Unlike</a>&nbsp";}
+                            echo "</font>";
+
+                            echo "<font size='1'><br><br></font>";
+            echo        "</span>";
+            echo    "</div>";          
+                        }
+                    }
+            echo "</div><br><br>";
         }
 
         echo "Showing posts " . (($page_number * 10) + 1) . "-" . (($page_number + 1) * 10) . " of " . $posts_count . "&nbsp;<a href=?page=" . ($page_number + 1) . ">Next page</a>";
